83c6a922b359f4dab63112e8a803939b9fe063a5
Isaac <isaavila@csumb.edu>
2019-09-16 20:05:30 -0700

    add flix files

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,32 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Isaac Avila on 9/16/19.
+//  Copyright © 2019 Isaac Avila. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
