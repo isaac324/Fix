181f938847e749ec33e5b0b829b6e1d8f67aefeb
Isaac <isaavila@csumb.edu>
2019-09-23 18:02:29 -0700

    add flix files

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Isaac Avila on 9/23/19.
+//  Copyright © 2019 Isaac Avila. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
